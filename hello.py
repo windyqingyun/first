aa140df41a700245f0e2b9adeacef6ce4bb27b29
windyqingyun <121253606@qqq.com>
2017-08-14 17:40:36 +0800

    first push

---
+++ hello.py
@@ -0,0 +1,40 @@
+from flask_moment import Moment
+from flask import Flask,render_template,session ,redirect,url_for,flash
+from datetime import datetime
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField , SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+	name = StringField('hello ,your name?',validators=[Required()])
+	submit = SubmitField('Submit')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ni cai bu dao de'
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
+@app.route("/",methods=['GET','POST'])
+def index(): 
+	#name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		#name = form.name.data
+		old_name = session.get("name")
+		if old_name is not None and old_name != form.name.data:
+			flash('ni shu de shi shen me gui dongxi ?')
+			flash('aaaaaaaaaaaaaaaa')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form = form , name = session.get('name') ,current_time = datetime.utcnow())
+
+@app.route('/home')
+def home():
+	return render_template('404.html')
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
+
+
